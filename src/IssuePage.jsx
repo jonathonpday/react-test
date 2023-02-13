9d32291164d5adc23379b2914e317071a8e79945
jonathonpday <jonathonpday@me.com>
2023-02-13 02:42:29 +0000

    Initial basic version completed

--- src/IssuePage.jsx
+++ src/IssuePage.jsx
@@ -16,7 +16,55 @@ const IssuePage = ({match}) => {
     <>
       {
         <>
-          <h1>Name: {issue.name}</h1>
+        <ul>
+          <li><strong>Name:</strong> {issue.name}</li>
+          <li><strong>Severity:</strong> {issue.severity}</li>
+          <li><strong>Category:</strong> {issue.category}</li>
+          <li><strong>Description:</strong> {issue.description}</li>
+          <li><strong>Remediation:</strong> {issue.remediation}</li>
+          <li><strong>Exploit Available:</strong> {issue.exploit_available}</li>
+          <li><strong>CVSS Score:</strong> {issue.cvss_score}</li>
+          <li><strong>CVSS3 Score:</strong> {issue.cvss3_score}</li>
+          <li>
+            <strong>CVES:</strong>
+            {issue.cves && (
+              <ul>
+                {issue.cves.map((cve, index) => (
+                  <li key={index}>{cve}</li>
+                ))}
+              </ul>
+            )}
+          </li>
+          <li>
+            <strong>External Links:</strong>
+            {issue.external_links && (
+              <ul>
+                {Object.values(issue.external_links).map((value, index) => (
+                  <li key={index}>
+                    <em>URL:</em> <a href={value.url} target="_blank">{value.url}</a>
+                    <br />
+                    <em>Title:</em> {value.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+          <li><strong>Nessus Plugin family:</strong> {issue.nessus_plugin_id}</li>
+          <li><strong>Created at:</strong> {issue.created_at}</li>
+          <li><strong>Updated at:</strong> {issue.updated_at}</li>
+          <li><strong>Deleted at:</strong> {issue.deleted_at}</li>
+          <li>
+            <strong>Additional Affected Assest Fields:</strong>
+            {issue.additional_affected_asset_fields && (
+              <ul>
+                {issue.additional_affected_asset_fields.map((assets, index) => (
+                  <li key={index}>{assets}</li>
+                ))}
+              </ul>
+            )}
+          </li>
+          <li><strong>Record To Remove:</strong> {issue.record_to_remove}</li>
+        </ul>
           <Link to="/">Back to homepage</Link>
         </>
       }
